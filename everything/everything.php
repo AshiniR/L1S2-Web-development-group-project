12913eb11c0b6e4dc4a42e0763efa670794d9148
Roshana Isuranga <roshanaisuran2002@gmail.com>
2024-04-21 08:01:27 +0530

    test files

--- everything/everything.php
+++ everything/everything.php
@@ -4,9 +4,9 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="everything.css">
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="everything.css" />
     <title>everything</title>
 </head>
 
@@ -16,22 +16,21 @@
     </div>
 
     <div class="main-content">
-        <div class="part1">
-
-        </div>
+        <div class="part1"></div>
         <div class="part2">
             <div class="text">
                 <h1>EVERYTHING</h1>
                 <span>Browse every items in our shop...</span>
             </div>
             <div class="items">
-
-                <div class="item-box"></div>
-                <div class="item-box"></div>
-                <div class="item-box"></div>
-                <div class="item-box"></div>
-                <div class="item-box"></div>
-
+                <div class="item-box">
+                    <div class="image"></div>
+                    <div class="details">
+                        <span class="name">Name</span><br>
+                        <span class="category">men</span><br>
+                        <span class="price">price</span>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
