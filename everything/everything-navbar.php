3cafae64e8c1f2774f9e23bb514b97a5ed20b8ce
Roshana Isuranga <roshanaisuran2002@gmail.com>
2024-04-22 07:09:14 +0530

    search bar function added for everything page and home page

--- everything/everything-navbar.php
+++ everything/everything-navbar.php
@@ -151,14 +151,6 @@
                     <a href="../men/men.php" class="nav-bar-links">MEN</a>
                     <a href="../about.php" class="nav-bar-links-about">ABOUT</a>
                     <a href="../contacts.php" class="nav-bar-links-contact">CONTACT US</a>
-                    <div class="nav-search">
-                        <form role="search" id="form">
-                            <input type="search" id="query" name="q" placeholder="Search..." aria-label="Search through site content">
-                            <button>
-                            <svg viewBox="0 0 1024 1024"><path class="path1" d="M848.471 928l-263.059-263.059c-48.941 36.706-110.118 55.059-177.412 55.059-171.294 0-312-140.706-312-312s140.706-312 312-312c171.294 0 312 140.706 312 312 0 67.294-24.471 128.471-55.059 177.412l263.059 263.059-79.529 79.529zM189.623 408.078c0 121.364 97.091 218.455 218.455 218.455s218.455-97.091 218.455-218.455c0-121.364-103.159-218.455-218.455-218.455-121.364 0-218.455 97.091-218.455 218.455z"></path></svg>
-                            </button>
-                        </form>
-                    </div>
                     <div class="nav-bar-icons">
                         <button href="" class="nav-bar-night" title="dark mode">
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-moon-stars-fill" viewBox="0 0 16 16">
--- everything/everything.php
+++ everything/everything.php
@@ -16,21 +16,61 @@
     </div>
 
     <div class="main-content">
-        <div class="part1"></div>
+
+        <div class="part1">
+            <div class="page_filter">
+                <form role="search" id="form1" class="page_search">
+                    <input type="search" id="query" name="search" placeholder="Search..." aria-label="Search through site content">
+                    <button>
+                        <svg viewBox="0 0 1024 1024">
+                            <path class="path1" d="M848.471 928l-263.059-263.059c-48.941 36.706-110.118 55.059-177.412 55.059-171.294 0-312-140.706-312-312s140.706-312 312-312c171.294 0 312 140.706 312 312 0 67.294-24.471 128.471-55.059 177.412l263.059 263.059-79.529 79.529zM189.623 408.078c0 121.364 97.091 218.455 218.455 218.455s218.455-97.091 218.455-218.455c0-121.364-103.159-218.455-218.455-218.455-121.364 0-218.455 97.091-218.455 218.455z"></path>
+                        </svg>
+                    </button>
+                </form>
+
+                <span class="filter-text">Filter Items</span>
+                <form id="form2" class="filter">
+                    <input type="text" name="" id="">
+                </form>
+            </div>
+
+        </div>
+
         <div class="part2">
             <div class="text">
                 <h1>EVERYTHING</h1>
                 <span>Browse every items in our shop...</span>
             </div>
             <div class="items">
-                <div class="item-box">
-                    <div class="image"></div>
-                    <div class="details">
-                        <span class="name">Name</span><br>
-                        <span class="category">men</span><br>
-                        <span class="price">price</span>
-                    </div>
-                </div>
+                <?php
+                if (isset($_GET['search']) && !empty($_GET['search'])) {
+                    $search_text = $conn->real_escape_string($_GET['search']);
+                    $sql = "SELECT * FROM men WHERE name LIKE '%$search_text%'";
+                } else {
+                    $sql = "SELECT * FROM men";
+                }
+                $result = $conn->query($sql);
+                if ($result->num_rows > 0) {
+                    while ($row = $result->fetch_assoc()) {
+                        $name = $row['name'];
+                        $img = $row['img'];
+                        $tag = $row['id'];
+                        echo "
+                        <div class='item-box'>
+                            <div class='image' id='image$tag'></div>
+                            <div class='details'>
+                                <span class='name'>$name</span><br>
+                                <span class='category'>men</span><br>
+                                <span class='price'>price</span>
+                            </div>
+                        </div>
+                        <script>
+                            document.getElementById('image$tag').style.backgroundImage = \"url('$img')\";
+                        </script>
+                            ";
+                    }
+                }
+                ?>
             </div>
         </div>
     </div>
